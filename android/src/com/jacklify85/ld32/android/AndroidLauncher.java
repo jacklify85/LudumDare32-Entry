1674656075869e808b938bb49e474e6ced8aa902
jacklify85 <jacklify85@users.noreply.github.com>
2015-04-17 21:14:14 -0400

    Initial Skeletal Project Generation

---
+++ android/src/com/jacklify85/ld32/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jacklify85.ld32.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jacklify85.ld32.LDGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LDGame(), config);
+	}
+}
---
+++ desktop/src/com/jacklify85/ld32/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jacklify85.ld32.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jacklify85.ld32.LDGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LDGame(), config);
+	}
+}
